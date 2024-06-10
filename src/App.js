4de6df1d9b78ae5c2cbd127ced372a66af40b2f8
Rishika goyal <rishikagoyal175@gmail.com>
2024-06-10 16:24:21 +0530

    name display

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import FullName from './FullName';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <FullName/>
+    </>
+    
+  );
+}
+
+export default App;
---
+++ src/FullName.js
@@ -0,0 +1,24 @@
+import React,{useState} from 'react';
+
+const FullName=()=>{
+    const[firstName,setFirstName]=useState("");
+    const[lastName,setLastName]=useState("");
+    const[clicked,setClicked]=useState(false);
+    return(
+        <>
+        <h2>Full Name Display</h2>
+        <form>
+         <label>First Name: </label>
+         <input type="text" onChange={(e)=>{setFirstName(e.target.value)}}required/><br></br>
+         <label>Last Name: </label>
+         <input type="text" onChange={(e)=>{setLastName(e.target.value)}} required/><br></br>
+         <button onClick={()=>{setClicked(true)}}>Submit</button>
+
+         {(clicked)&& (firstName!=="" && lastName!=="")?(<><p>Full Name: <span>{firstName+" "+lastName}</span></p></>):(<></>)}
+
+        </form>
+        
+        </>
+    )
+}
+export default FullName;
